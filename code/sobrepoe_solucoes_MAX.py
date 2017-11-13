064d67c4220449e1600db55adbfaacc3f71077fb
Samuel Terra <samuelterra22@hotmail.com>
2017-11-13 03:35:09 -0200

    Revisao do pdf feita.

---
+++ code/sobrepoe_solucoes_MAX.py
@@ -0,0 +1,5 @@
+def sobrepoe_solucoes_MAX(propagation_array, size):
+    max = propagation_array[0]
+    for i in range(1, size):
+        max = np.maximum(propagation_array[i], max)
+    return max
